ae000ad1a518c33658e06e425fff9a02ce390758
narcisbuzatu2003@gmail.com <narcisbuzatu2003@gmail.com>
2021-03-02 19:20:13 +0100

    Aggiungere i file di progetto.

---
+++ Es1Classi/Riga.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es1Classi
+{
+    public class Riga
+    {
+        List<Cella> celle = new List<Cella>();
+
+        public Riga() { }
+
+        public Cella NewCella()
+        {
+            Cella c = new Cella();
+            celle.Add(c);
+            return c;
+        }
+    }
+}
